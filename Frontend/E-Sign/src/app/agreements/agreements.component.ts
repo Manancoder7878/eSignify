c9795998019d86a2e633ee0d7caaddb8113b36c1
Manan B. Prajapati <mananprajapati957@gmail.com>
2025-06-22 15:27:30 +0530

    Frontend new icons

--- Frontend/E-Sign/src/app/agreements/agreements.component.ts
+++ Frontend/E-Sign/src/app/agreements/agreements.component.ts
@@ -1,11 +1,74 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-agreements',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './agreements.component.html',
   styleUrl: './agreements.component.css'
 })
 export class AgreementsComponent {
+  documents = [
+    {
+      title: 'Abstract 3D',
+      desc: 'Lorem ipsum dolor sit amet...',
+      status: 'Signed',
+      price: '$45.99',
+      date: '20',
+      image: 'assets/doc1.png'
+    },
+    {
+      title: 'Landing Page 3D max',
+      desc: 'Lorem ipsum dolor sit amet...',
+      status: 'Unsigned',
+      price: '$45.99',
+      date: '20',
+      image: 'assets/doc2.png'
+    },
+    {
+      title: 'Landing Page 3D max',
+      desc: 'Lorem ipsum dolor sit amet...',
+      status: 'Unsigned',
+      price: '$45.99',
+      date: '20',
+      image: 'assets/doc2.png'
+    },
+    {
+      title: 'Landing Page 3D max',
+      desc: 'Lorem ipsum dolor sit amet...',
+      status: 'Unsigned',
+      price: '$45.99',
+      date: '20',
+      image: 'assets/doc2.png'
+    }
+    // Add other documents similarly
+  ];
+  metrics = [
+    {
+      label: 'Total Documents',
+      value: '$198k',
+      bgColor: '#E8FFF0',  // light green
+      circleColor: '#A4F3C3',
+      
+    },
+    {
+      label: 'Signed',
+      value: '$2.4k',
+      bgColor: '#EAF7FF',  // light blue
+      circleColor: '#BFE7FF',
+      
 
+    },
+    {
+      label: 'Unsigned',
+      value: '$89k',
+      bgColor: '#FFE9F0',  // light pink
+      circleColor: '#FFB6C9',
+    }
+  ];
+
+  getStatusClass(status: string) {
+    return status === 'Signed' ? 'bg-succes rounded-2' : 'bg-danger rounded-2';
+  }
 }
